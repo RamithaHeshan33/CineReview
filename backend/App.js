604884ac8c646937ded554489a8e039834233984
Ramitha Heshan <st20286184@outlook.cardiffmet.ac.uk>
2025-01-31 00:57:51 +0530

    connected to the database

--- backend/App.js
+++ backend/App.js
@@ -1,1 +1,16 @@
-console.log('Apps');
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+//Middleware
+app.use(express.json());
+app.use('/', (res, req, next) => {
+    res.send('Hello World');
+});
+
+//Connect to DB
+//IPPLJ26NbFbtkpkJ
+mongoose.connect("mongodb+srv://admin:IPPLJ26NbFbtkpkJ@cluster0.zyjul.mongodb.net/")
+.then(() => console.log('Connected to DB'))
+.then(() => {app.listen(3000);})
+.catch(err => console.log(err));
